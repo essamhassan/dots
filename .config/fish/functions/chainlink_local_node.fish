28d857d037fc3067ebd2bda8a545ea4fa549a017
Sam Hassan <es.hassan187@gmail.com>
2022-02-08 16:23:19 +0100

    modify fish funcs for local chainlink node

--- .config/fish/functions/chainlink_local_node.fish
+++ .config/fish/functions/chainlink_local_node.fish
@@ -2,5 +2,5 @@ function chainlink_local_node
 run_chainlink_pgs_local 
 set_chainlink_dev
 sleep 10
-chainlink node start
+chainlink node start -p ~/.chainlink_pwd
 end
--- .config/fish/functions/set_chainlink_dev.fish
+++ .config/fish/functions/set_chainlink_dev.fish
@@ -8,4 +8,9 @@ replay export ETH_CHAIN_ID=4
 replay export CHAINLINK_TLS_PORT=0
 replay export SECURE_COOKIES=false
 replay export ALLOW_ORIGINS='*'
+replay export ETH_URL=ws://127.0.0.1:8546
+replay export GAS_UPDATER_ENABLED=true
+replay export DATABASE_TIMEOUT=0
+replay export MIN_OUTGOING_CONFIRMATIONS=2
+replay export LINK_CONTRACT_ADDRESS=0x01BE23585060835E02B77ef475b0Cc51aA1e0709
 end
