81146ae5137dd7e9f186a737133ef18c717bcf37
Sam Hassan <es.hassan187@gmail.com>
2022-04-04 18:23:56 +0200

    add ident for fish func

--- .config/fish/functions/run_chainlink_pgs_local.fish
+++ .config/fish/functions/run_chainlink_pgs_local.fish
@@ -1,10 +1,10 @@
 function run_chainlink_pgs_local
-sudo docker stop postgres
-sudo docker rm postgres
-sudo docker run -d -p 5432:5432 --name postgres \
- -e POSTGRES_USER=samhassan \
- -e POSTGRES_DB=chainlink \
- -e POSTGRES_HOST_AUTH_METHOD=trust \
- -v pgdata:/var/lib/postgresql/data \
- postgres:11
+  sudo docker stop postgres
+  sudo docker rm postgres
+  sudo docker run -d -p 5432:5432 --name postgres \
+    -e POSTGRES_USER=samhassan \
+    -e POSTGRES_DB=chainlink \
+    -e POSTGRES_HOST_AUTH_METHOD=trust \
+    -v pgdata:/var/lib/postgresql/data \
+    postgres:11
 end
