74937b8c728376656689b6d98048999515f4db75
Sam Hassan <es.hassan187@gmail.com>
2023-03-28 16:43:38 +0200

    rm legacy cl stuff, rm vim, modify i3

--- .config/fish/functions/chainlink_local_node.fish
+++
@@ -1,6 +0,0 @@
-function chainlink_local_node
-run_chainlink_pgs_local 
-set_chainlink_dev
-sleep 10
-chainlink node start -p ~/.chainlink_pwd
-end
--- .config/fish/functions/run_local_eth.fish
+++ .config/fish/functions/run_local_eth.fish
@@ -1,4 +1,4 @@
 function run_local_eth
    set_chainlink_dev
-   geth --rinkeby --ws --ws.addr 127.0.0.1 --ws.port 8546 --ws.origins "*"
+   geth --cache 2048 --rinkeby --ws --ws.addr 127.0.0.1 --ws.port 8546 --ws.origins "*"
 end
